e0a27a8e45207bdc8c9d9952b3193162fdae386d
RAO <daulatrao.patil@walchandsangli.ac.in>
2022-07-08 11:16:44 +0530

    Day 3: Question 1

---
+++ Data Structure I/Day 3/IntersectionOfTwoArraysII.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define endl "\n" 
+#define MOD 1000000007
+
+/*    Ignore    */
+void solve(){
+    
+    
+    return;
+}
+
+int main(){
+    fastIO
+    ll t;
+    cin>>t;
+           
+    while(t--){
+        solve();
+    }
+              
+    return 0;
+}
