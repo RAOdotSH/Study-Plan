5611bfcad0f6673dabed84c5965fecfcb9818acd
RAOexe <2020bteit00034@walchandsangli.ac.in>
2022-11-08 09:29:59 +0530

    Day 1 Que 1

---
+++ SQL I/Day 1/BigCountries.sql
@@ -0,0 +1,6 @@
+-- Problem: https://github.com/RAOexe/Study-Plan/blob/main/SQL%20I/README.md#big-countries
+
+-- Code
+SELECT name, population, area
+FROM World
+WHERE area >= 3000000 OR population >= 25000000
