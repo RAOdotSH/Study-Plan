0dd73c823851f3598bdfd48cd161a39b872f3d7b
RAOexe <2020bteit00034@walchandsangli.ac.in>
2022-11-08 09:52:34 +0530

    Day 1 Que 2

---
+++ SQL I/Day 1/RecyclableAndLowFatProducts.sql
@@ -0,0 +1,8 @@
+-- Problem: https://github.com/RAOexe/Study-Plan/blob/main/SQL%20I/Day%20I/README.md#
+
+-- Code
+
+# Write your MySQL query statement below
+SELECT product_id
+FROM Products
+WHERE low_fats = "Y" AND recyclable = "Y"
